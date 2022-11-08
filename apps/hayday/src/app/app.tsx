645aec853de08aa5100bcd003ccef2813d24266d
Stephanie Zeng <stephanie.zeng@rangle.io>
2022-11-08 17:18:51 -0500

    Use button component in app

--- apps/hayday/src/app/app.tsx
+++ apps/hayday/src/app/app.tsx
@@ -1,12 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
+import { Button } from 'libs/ui/src/index';
 
 export function App() {
   return (
     <>
       <NxWelcome title="hayday" />
       <div />
+      <Button id="test"/>
     </>
   );
 }
---
+++ libs/index.ts
@@ -0,0 +1,2 @@
+export * from './ui/src/lib/button/button';
+export * from './ui/src/lib/ui';
