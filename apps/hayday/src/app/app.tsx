d6050a777834c3e972aaf8f5bfcd965d1c91bbea
Stephanie Zeng <stephanie.zeng@rangle.io>
2022-11-08 19:01:31 -0500

    Update button import in app.tsx

--- apps/hayday/src/app/app.tsx
+++ apps/hayday/src/app/app.tsx
@@ -1,7 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
-import { Button } from 'libs/ui/src/index';
+import { Button } from '@hay/ui';
+
 
 export function App() {
   return (
